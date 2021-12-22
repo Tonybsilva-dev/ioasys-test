97af1e345d04ca23ebc13a26625e618b833383e8
Antonio Bernardino da Silva <antoniobernardinodasilva@Antonio-S.local>
2021-12-22 12:13:08 -0300

    Product module add

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { accessControlRouter } from '@modules/accessControlList/infra/http/accessControl.routes';
 import { permissionsRouter } from '@modules/accessControlList/infra/http/permissions.routes';
 import { rolesRouter } from '@modules/accessControlList/infra/http/roles.routes';
+import { productsRouter } from '@modules/products/infra/http/products.routes';
 import { sessionsRouter } from '@modules/users/infra/http/sessions.routes';
 import { userRouter } from '@modules/users/infra/http/users.routes';
 import { Router } from 'express';
@@ -12,5 +13,6 @@ routes.use('/sessions', sessionsRouter)
 routes.use('/roles', rolesRouter)
 routes.use('/permissions', permissionsRouter)
 routes.use('/access_control', accessControlRouter)
+routes.use('/products', productsRouter)
 
 export default routes;
